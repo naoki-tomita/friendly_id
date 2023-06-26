fa69d243bf26eab2f198074d30607d54c8ad1f77
Naoki Tomita🐱 <mxcpy608@hotmail.com>
2023-06-27 08:27:36 +0900

    Update uuid and wasm-bindgen feature

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,3 @@ pub use self::friendly_id::{create, encode, decode};
 mod base62;
 mod friendly_id;
 mod error;
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
